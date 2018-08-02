143c371a425d46667dfd0a330905154afa6b821a
kinchiller <kingchiller@gmail.com>
2018-08-02 08:42:53 +0200

    Basic hero component

---
+++ components/Hero.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="hero">
+        <img class="hero__background" :src="backgroundImage" />
+        <div class="hero__content">
+          <slot></slot>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  props: {
+    backgroundImage: {
+      type: String,
+      default: '/images/background-drones.jpg'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.hero {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    min-height: calc(100vh - #{$header-height});
+    position: relative;
+
+    .hero__background {
+        left: 0;
+        height: 100%;
+        object-fit: cover;
+        position: absolute;
+        top: 0;
+        width: 100%;
+    }
+
+    &::after {
+        background-color: rgba(0, 0, 0, 0.2);
+        content: '';
+        left: 0;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        width: 100%;
+    }
+
+    .hero__content {
+      color: white;
+      position: relative;
+      max-width: 90%;
+      z-index: 1;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
 <div class="index">
+  <Hero>
+    <h1>Insure your drone now!</h1>
+  </Hero>
   <Section>
     <h1>Simpego - smart insurance</h1>
     <div class="section__headline">Switerland's first insurance marketplace. We did the comparison for you. Eliminate risk at the lowest rates.</div>
@@ -19,12 +22,27 @@
 
 <script>
 import Button from '../components/Button'
+import Hero from '../components/Hero'
 import Section from '../components/Section'
 
 export default {
   components: {
     Button,
+    Hero,
     Section
   }
 }
 </script>
+
+<style lang="scss">
+.hero {
+  h1 {
+    font-size: 2em;
+    text-align: center;
+
+    @media screen and (min-width: $breakpointSmall) {
+      font-size: 3em;
+    }
+  }
+}
+</style>
